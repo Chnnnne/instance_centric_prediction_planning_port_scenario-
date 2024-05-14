fix bug in gt ref and add masked softmax in traj_prob lyaer

common/plot_utils.py
@@ -230,10 +230,10 @@ def draw_candidate_refpaths_with_his_fut(ori, candidate_refpaths, cand_gt_idx = 
     traj_all = np.concatenate((his_traj, fut_traj), axis=0)
     roi_matrix = get_xy_lim(ori, points_xy=(traj_all[:,0], traj_all[:,1]))
     ax.axis(roi_matrix)
-    if his_traj != None:
+    if type(his_traj) == np.ndarray:
         # history
         ax.plot(his_traj[:,0], his_traj[:,1], color='red',zorder=15,alpha= 0.5)
-    if fut_traj != None:
+    if type(fut_traj) == np.ndarray:
         # fut
         ax.plot(fut_traj[:,0], fut_traj[:,1], marker='x', linestyle="--", color="green",alpha=0.5)
         # fut 5s point

data_process/generate_feat_multi.py
@@ -303,13 +303,11 @@ def generate_future_feats_path(data_info: dict, target_ids: list):
                 candidate_refpaths_vecs = np.zeros((1, 20, 2))#(1,20,2)
                 candts_mask = np.zeros((1))#(1, )
             else:
-                # plot_utils.draw_candidate_refpaths_with_his_fut(ori=ori,candidate_refpaths=candidate_refpaths_cords,cand_gt_idx=gt_idx,his_traj=agt_traj_his,fut_traj=agt_traj_fut_all)
+                plot_utils.draw_candidate_refpaths_with_his_fut(ori=ori,candidate_refpaths=candidate_refpaths_cords,cand_gt_idx=gt_idx,his_traj=agt_traj_his,fut_traj=agt_traj_fut_all)
                 gt_cand = np.zeros(len(candidate_refpaths_cords),dtype=np.int32) # (N,)
-                gt_cand[-1] = 1
+                gt_cand[gt_idx] = 1
                 candidate_refpaths_cords = [mp_seacher.sample_points(refpath_cords, num=20,return_content="points") for refpath_cords in candidate_refpaths_cords] # list[ndarray:shape(50,2)] ->  (N, 20, 2)
                 candidate_refpaths_vecs = mp_seacher.get_refpath_vec(candidate_refpaths_cords) #list[ndarray] (N, 20, 2)
-                # ！转化坐标系！！！！！！！！！！！！！
-                # candidate_refpaths_cords = transform_to_local_coords(candidate_refpaths_cords,center_xy,center_heading)
                 candidate_refpaths_vecs = np.asarray(candidate_refpaths_vecs)# (N, 20, 2)
                 candidate_refpaths_cords = [transform_to_local_coords(refpath_cord, center_xy, center_heading) for refpath_cord in candidate_refpaths_cords]
                 candidate_refpaths_cords = np.asarray(candidate_refpaths_cords) # (N, 20, 2)
@@ -758,8 +756,8 @@ if __name__=="__main__":
     hdmap = HDMapManager.GetHDMap()
     mp_seacher = MapPointSeacher(hdmap, t=5.0)
     
-    input_path = '/private2/wanggang/pre_log_inter_data'
-    # input_path = '/private/wangchen/pre_log_inter_data_small'
+    # input_path = '/private2/wanggang/pre_log_inter_data'
+    input_path = '/private/wangchen/instance_model/pre_log_inter_data_small'
     all_file_list = [os.path.join(input_path, file) for file in os.listdir(input_path)]
     all_file_list = all_file_list[:int(len(all_file_list)/5)]
     train_files, test_files = train_test_split(all_file_list, test_size=0.2, random_state=42)
@@ -772,8 +770,10 @@ if __name__=="__main__":
     if not cur_output_path.exists():
         cur_output_path.mkdir(parents=True)
 
-    pool = multiprocessing.Pool(processes=16)
-    pool.map(load_seq_save_features, range(len(cur_files)))
+    load_seq_save_features(index= -1, file_path="/private/wangchen/instance_model/instance_model_data/test/howo32_1683498380.743353.pkl")
+    # pool = multiprocessing.Pool(processes=16)
+    # pool.map(load_seq_save_features, range(len(cur_files)))
+
     # for i in range(len(cur_files)): # 19 error 21 draw
     #     print("--"*20, i)
     # #     # my_candidate_refpath_search_test(i)

instance_centric_model/src/layers/traj_decoder.py
@@ -77,11 +77,18 @@ class TrajDecoder(nn.Module):
 
     def forward(self, feats, candidate_refpaths_cords, candidate_refpaths_vecs, gt_refpath, candidate_mask=None,batch_dict = None):
         """
-        feats: B,N,D                                                                           [b,all_n-Max, d_agent]
-        candidate_refpaths_cords:   B,N,M,20,2                                                           [b,all_n-Max,Max-N-Max, 20, 2] 
-        candidate_refpaths_vecs: B,N,M,20,2  N个agent ， 每个agent共M的采样点（M是最大的）            [b,all_n-Max,Max-N-Max, 20, 2]  
-        gt_refpath:  B, N, M                真值refpath one hot                                 [b, all_n-Max, Max-N-Max]
-        candidate_mask: B, N, M             因为每个agent的采样点数量不同，因此 M标记哪些是和不是      [b,all_n-Max,Max-N-Max]
+        input:
+            - feats: B,N,D                                                                           [b,all_n-Max, d_agent]
+            - candidate_refpaths_cords:   B,N,M,20,2                                                           [b,all_n-Max,Max-N-Max, 20, 2] 
+            - candidate_refpaths_vecs: B,N,M,20,2  N个agent ， 每个agent共M的采样点（M是最大的）            [b,all_n-Max,Max-N-Max, 20, 2]  
+            - gt_refpath:  B, N, M                真值refpath one hot                                 [b, all_n-Max, Max-N-Max]
+            - candidate_mask: B, N, M             因为每个agent的采样点数量不同，因此 M标记哪些是和不是      [b,all_n-Max,Max-N-Max]
+        
+        output:
+            - cand_refpath_probs:   B,N,M 
+            - param:    B,N,M,(n_order+1)*2
+            - traj_probs:       B, N, M
+            - param_with_gt:     B,N,1,(n_order+1)*2
         """
         #可视化验证
         # self.vis_debug(candidate_refpaths_cords=candidate_refpaths_cords, candidate_refpaths_vecs=candidate_refpaths_vecs,gt_refpath=gt_refpath,candidate_mask=candidate_mask,batch_dict=batch_dict)
@@ -89,14 +96,14 @@ class TrajDecoder(nn.Module):
         feats_repeat = feats.unsqueeze(2).repeat(1, 1, M, 1) # B, N, M, D
         feats_cand = torch.cat([feats_repeat, candidate_refpaths_cords.reshape(B,N,M,-1), candidate_refpaths_vecs.reshape(B,N,M,-1)], dim=-1) # B,N,M, D+40+40
         prob_tensor = self.cand_refpath_prob_layer(feats_cand).squeeze(-1) # B,N,M, D+40+40 -> B,N,M 很多空的D 4040送入预测器
-        cand_refpath_probs = self.masked_softmax(prob_tensor, candidate_mask, dim=-1) # B,N,M + B,N,M -> B,N,M 空数据的概率为0
+        cand_refpath_probs = self.masked_softmax(prob_tensor, candidate_mask, dim=-1) # B,N,M + B,N,M -> B,N,M 空数据被mask为概率0
 
         param_input = feats_cand  # B,N,M, D+40+40
-        param = self.motion_estimator_layer(param_input) # B,N,M,(n_order+1)*2 空的数据也会预测轨迹
+        param = self.motion_estimator_layer(param_input) # B,N,M,(n_order+1)*2 空的数据也会预测轨迹，可由下面的prob做mask，因为prob为0的轨迹忽略
 
         prob_input = torch.cat([feats_repeat, param], dim=-1) # B, N, M, D + (n_order+1)*2
-        traj_probs = self.traj_prob_layer(prob_input).squeeze(-1) # B, N, M 打分 空的数据也会打分，没有做mask
-        
+        traj_prob_tensor = self.traj_prob_layer(prob_input).squeeze(-1) # B, N, M 打分 空的parm和特征数据也会打分，做了softmax但没做mask，因此没mask的位置也会有概率评分
+        traj_probs = self.masked_softmax(traj_prob_tensor, candidate_mask, dim = -1) # B,N,M + B,N,M
         # 预测轨迹(teacher_force)
         # feat_traj_with_gt = torch.cat([feats.unsqueeze(2), target_gt], dim=-1) # B, N, 1, D+2
         # param_with_gt = self.motion_estimator_layer(feat_traj_with_gt) # B,N,1,(n_order+1)*2

instance_centric_model/src/loss.py
@@ -18,14 +18,16 @@ class Loss(nn.Module):
     
     def forward(self, input_dict, output_dict, epoch=1):
         loss = 0.0
-        pred_mask = (input_dict['candidate_mask'].sum(dim=-1) > 0).cuda() # B,N,M->B, N  仅标志哪个agent是有效的，不标志refpath有效
-        # 1、target_loss
+        candidate_mask = input_dict['candidate_mask'] # B,N,M
+        pred_mask = (candidate_mask.sum(dim=-1) > 0).cuda() # B,N,M->B, N  仅标志哪个agent是有效的，不标志refpath有效
+        s_candidate_mask = candidate_mask[pred_mask].cuda() # S, M
+        # 1、ref path cls loss
         gt_probs = input_dict['gt_candts'].float().cuda() # B, N, M     
 
-        gt_probs = gt_probs[pred_mask] # S, M   s个agent 每个agentM个refpath     
-        pred_probs = output_dict['cand_refpath_probs'][pred_mask] # B, N, M -> S, M    s个agent，m个预测点
+        gt_probs = gt_probs[pred_mask] # S, M   s个agent, 每个agent M个refpath     
+        pred_probs = output_dict['cand_refpath_probs'][pred_mask] # B, N, M -> S, M （s个agent，m个预测点） 被mask住的数据，model预测已调整为0，
         pred_num = pred_probs.shape[0] # S
-        cls_loss = F.binary_cross_entropy(pred_probs, gt_probs, reduction='sum')/pred_num 
+        cls_loss = F.binary_cross_entropy(pred_probs, gt_probs, reduction='sum')/pred_num  # 对于被mask的数据项，根据BSE定义，得单项loss为0，因此此处的loss无需再做mask
         
         # gt_tar_offset = input_dict["gt_tar_offset"].cuda() # B, N ,2
         # try:
@@ -52,13 +54,14 @@ class Loss(nn.Module):
         reg_loss = F.smooth_l1_loss(traj_with_gt, gt_trajs, reduction="sum")/pred_num
         
         # 3、score_loss
+        #由于要对 【模型对每条预测轨迹的打分器】 进行训练，因此我们就需要(打分器对每条轨迹的打分(其实是概率值))以及(真值)作为计算Loss的输入
         pred_trajs = output_dict['trajs'][pred_mask] # B,N,M,50,2 -> S, M, 50, 2
         S, m, horizon, dim = pred_trajs.shape
         pred_trajs = pred_trajs.view(S, m , horizon*dim) # S,M, 100
         gt_trajs = gt_trajs.view(S, horizon*dim)# S, 50, 2 -> S, 100
-        score_gt = F.softmax(-self.distance_metric(pred_trajs,  gt_trajs)/self.temper, dim=-1).detach()# S,m
-        score_loss = F.binary_cross_entropy(output_dict['traj_probs'][pred_mask], score_gt, reduction='sum')/pred_num  # SM 
-        #由于要对 【模型对每条预测轨迹的打分器】 进行训练，因此我们就需要(打分器对每条轨迹的打分(其实是概率值))以及(真值)作为计算Loss的输入
+        # score_gt = F.softmax(-self.distance_metric(pred_trajs,  gt_trajs)/self.temper, dim=-1).detach()# S,m
+        score_gt = self.masked_softmax(vector=-self.distance_metric(pred_trajs,  gt_trajs)/self.temper,mask=s_candidate_mask).detach()
+        score_loss = F.binary_cross_entropy(output_dict['traj_probs'][pred_mask], score_gt, reduction='sum')/pred_num  # S M   model输出的traj_probs对于mask数据已调整为0，score_gt对mask的数据也也做处理，因此此处可直接算BCE
 
         
         if epoch > 10:
@@ -194,3 +197,23 @@ class Loss(nn.Module):
         # weight = torch.linspace(0.5,1.5,int(horizon_2_times / 2)).cuda()
         # res = torch.sum(dis*weight, axis = 2)
         return dis
+    
+    def masked_softmax(self, vector, mask, dim=-1, memory_efficient=True, mask_fill_value=-1e32):
+        # vector = mask = BNM
+        if mask is None:
+            result = F.softmax(vector, dim=dim)
+        else:
+            mask = mask.float()
+            while mask.dim() < vector.dim():
+                mask = mask.unsqueeze(-1)
+            if not memory_efficient:
+                # To limit numerical errors from large vector elements outside the mask, we zero these out.
+                result = F.softmax(vector * mask, dim=dim)
+                result = result * mask
+                result = result / (result.sum(dim=dim, keepdim=True) + 1e-13)
+                result = result.masked_fill((1 - mask).bool(), 0.0)
+            else:
+                masked_vector = vector.masked_fill((1 - mask).bool(), mask_fill_value) # 将BNM中没有被mask住的位置置为负无穷
+                result = F.softmax(masked_vector, dim=dim) # 做softmax得到概率
+                result = result.masked_fill((1 - mask).bool(), 0.0) # BNM没被mask住的位置概率置为0
+        return result

instance_centric_model/src/model.py
@@ -125,25 +125,22 @@ class Model(nn.Module):
         target_gt = batch_dict['gt_preds'][:, :, -1, :2].cuda() # [b, all_n-Max, 2]
         target_gt = target_gt.view(target_gt.shape[0], target_gt.shape[1], 1, 2) # [b, all_n-Max, 1, 2]
         gt_refpath = batch_dict['gt_candts'] # [b, all_n-Max, Max-N-Max]
-        # target_probs, pred_targets, pred_offsets, param, param_with_gt, traj_probs = self.traj_decoder(agent_feats, candidate_refpaths_cords, candidate_refpaths_vecs, target_gt, candidate_mask)
         cand_refpath_probs, param, traj_probs, param_with_gt = self.traj_decoder(agent_feats, candidate_refpaths_cords, candidate_refpaths_vecs, gt_refpath, candidate_mask,batch_dict)
         # 由贝塞尔控制点反推轨迹
-        # bezier_param = torch.cat([param, pred_targets], dim=-1) # B, N, m, (n_order+1)*2
         bezier_control_points = param.view(param.shape[0],
                                            param.shape[1],
                                            param.shape[2], -1, 2) # # B,N,M,n_order*2 -> B, N, m, n_order+1, 2
         trajs = torch.matmul(self.mat_T, bezier_control_points) # B,N,m,future_steps,2
         
-        # # bezier_param_with_gt = torch.cat([param_with_gt, target_gt], dim=-1) # B, N, 1, (n_order+1)*2
         bezier_control_points_with_gt = param_with_gt.view(param_with_gt.shape[0],
                                                            param_with_gt.shape[1],
                                                            param_with_gt.shape[2], -1, 2) # B, N, 1, n_order+1*2 ->B, N, 1, n_order+1, 2
         traj_with_gt = torch.matmul(self.mat_T, bezier_control_points_with_gt) # B,N,1,future_steps,2
 
-        return {"cand_refpath_probs": cand_refpath_probs,
-                "traj_with_gt": traj_with_gt,
-                "trajs": trajs,
-                "traj_probs": traj_probs
+        return {"cand_refpath_probs": cand_refpath_probs, # B,N,M
+                "traj_with_gt": traj_with_gt,#B,N,1,50,2
+                "trajs": trajs, # B,N,M,50,2
+                "traj_probs": traj_probs # B,N,M
                }  
         
     def _get_T_matrix_bezier(self, n_order, n_step):